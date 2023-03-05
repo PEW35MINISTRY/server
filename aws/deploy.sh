4c289c74084aa5aef18dfc367dbf3a1516aec23a
Ethan Johnsrud <ethanjohnsrud@gmail.com>
2023-03-05 16:24:29 -0600

    Deploy includes fetch website and portal

--- aws/deploy.sh
+++ aws/deploy.sh
@@ -3,7 +3,7 @@ echo "Deploying Encouraging Prayer Server"
 cd /home/ubuntu/server
 
 #Delete dependnecies | Note: not deleting everything
-sudo rm -rf node_modules package-lock.json
+sudo rm -rf node_modules package-lock.json website portal
 
 #Update source code
 git reset --hard
@@ -15,6 +15,38 @@ npm install
 
 #Compile typescript
 npm run build
+
+#Update WEBSITE
+cd /home/ubuntu/website
+
+#Delete dependnecies
+sudo rm -rf node_modules package-lock.json build
+
+git reset --hard
+git pull origin release
+git checkout --force release
+
+npm install
+
+npm run build 
+
+cp ./build ../server/website
+
+#Update PORTAL
+cd /home/ubuntu/portal
+
+#Delete dependnecies
+sudo rm -rf node_modules package-lock.json build
+
+git reset --hard
+git pull origin release
+git checkout --force release
+
+npm install
+
+npm run build 
+
+cp ./build ../server/portal
 
 #start PM2
 cd 0-compiled
