b3f8c48bacdc1f2367c7114c70de598932a5b3fe
Ethan Johnsrud <ethanjohnsrud@gmail.com>
2023-04-22 19:56:01 -0500

    Configured Depoly Script with options

--- aws/deploy.sh
+++ aws/deploy.sh
@@ -1,59 +1,109 @@
 # Must enable permissions for npm scripts
 # sudo chmod 777 deploy.sh
-echo "Deploying Encouraging Prayer Server"
 
-cd /home/ubuntu/server
+#Update Server
+if [ "$#" -eq 0 ] || [[ "$*" == *"server"* ]]
+then
+    echo "[0/20] Deploying Encouraging Prayer Server"
+    cd /home/ubuntu/server
 
-#Delete dependnecies | Note: not deleting everything
-sudo rm -rf node_modules package-lock.json website portal
+    #Delete dependnecies | Note: not deleting everything
+    echo "[1/20] Deleteing Server Temperary Files"
+    sudo rm -rf node_modules package-lock.json
 
-#Update source code
-git reset --hard
-git pull origin release
-git checkout --force release
+    #Update source code
+    echo "[2/20] Updating Server Source Code"
+    git reset --hard
+    git pull origin release
+    git checkout --force release
 
-# Install latest dependencies
-npm install
+    # Install latest dependencies
+    echo "[3/20] Installing Server Dependencies"
+    npm install
 
-#Compile typescript
-npm run build
+    #Compile typescript
+    echo "[4/20] Compiling Server Typescript"
+    npm run build
+
+    echo "[5/20] Server Build Complete"
+
+else
+    echo "[5/20] Skipping Server Build"
+fi
 
 #Update WEBSITE
-cd /home/ubuntu/website
+if [ "$#" -eq 0 ] || [[ "$*" == *"website"* ]]
+then
+    echo "[6/20] Deploying Encouraging Prayer Website"
 
-#Delete dependnecies
-sudo rm -rf node_modules package-lock.json build
+    #Delete dependnecies
+    echo "[7/20] Deleteing Website Temperary Files"
+    cd /home/ubuntu/server
+    sudo rm -rf website
 
-git reset --hard
-git pull origin release
-git checkout --force release
+    cd /home/ubuntu/website
+    sudo rm -rf node_modules package-lock.json build
 
-npm install
+    #Update source code
+    echo "[8/20] Updating Website Source Code"
+    git reset --hard
+    git pull origin release
+    git checkout --force release
 
-npm run build
+    echo "[9/20] Installing Website Dependencies"
+    npm install
 
-cp ./build/ ../server/website/
+    echo "[10/20] Building Website Production Code"
+    npm run build
+
+    cp ./build/ ../server/website/
+
+    echo "[11/20] Website Build Complete"
+else
+    echo "[11/20] Skipping Website Build"
+fi
 
 #Update PORTAL
-cd /home/ubuntu/portal
+if [ "$#" -eq 0 ] || [[ "$*" == *"portal"* ]]
+then
+    echo "[12/20] Deploying Encouraging Prayer Portal"
 
-#Delete dependnecies
-sudo rm -rf node_modules package-lock.json build
+    #Delete dependnecies
+    echo "[13/20] Deleteing Portal Temperary Files"
+    cd /home/ubuntu/server
+    sudo rm -rf website portal
 
-git reset --hard
-git pull origin release
-git checkout --force release
+    cd /home/ubuntu/portal
+    sudo rm -rf node_modules package-lock.json build
 
-npm install
+    #Update source code
+    echo "[14/20] Updating Portal Source Code"
+    git reset --hard
+    git pull origin release
+    git checkout --force release
 
-npm run build
+    echo "[15/20] Installing Portal Dependencies"
+    npm install
 
-cp ./build/ ../server/portal/
+    echo "[16/20] Building Portal Production Code"
+    npm run build
 
+    cp ./build/ ../server/portal/
+
+    echo "[17/20] Portal Build Complete"
+
+else
+    echo "[17/20] Skipping Portal Build"
+fi
+
+echo "[18/20] Updating Server SSL Certificates"
 #Update SSL | chron auto updates 2023-6-17
 cp "~/etc/letsencrypt/live/encouragingprayer.org/fullchain.pem" "~/server/aws/fullchain.pem"
 cp "~/etc/letsencrypt/live/encouragingprayer.org/privkey.pem" "~/server/aws/privkey.pem"
 
+echo "[19/20] Copying pm2-start configuration settings"
 #start PM2
 cd 0-compiled
 sudo cp "/home/ubuntu/server/aws/pm2-start.json" "."
+
+echo "[20/20] Encouraging Prayer Production Build Complete"
